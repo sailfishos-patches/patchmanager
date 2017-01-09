ca45cc33999c455d23e1168a5b464d71214414cb
coderus <coderusinbox@gmail.com>
2017-01-09 14:25:14 +0300

    Udated translations

--- src/qml/translations/settings-patchmanagerts_ca.ts
+++ src/qml/translations/settings-patchmanagerts_ca.ts
@@ -1,16 +1,16 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sl_SI" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ca" version="2.1">
 <context>
     <name/>
     <message>
         <location filename="AboutPage.qml" line="48"/>
         <location filename="PatchManagerPage.qml" line="116"/>
         <source>About</source>
-        <translation>O programu</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="AboutPage.qml" line="62"/>
         <source>Patchmanager</source>
-        <translation>Patchmanager</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="AboutPage.qml" line="80"/>
@@ -21,13 +21,13 @@
         <location filename="AboutPage.qml" line="91"/>
         <location filename="WebPatchPage.qml" line="300"/>
         <source>Donate</source>
-        <translation>Prispevaj</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="AboutPage.qml" line="102"/>
         <location filename="DevelopersPage.qml" line="97"/>
         <source>Developers</source>
-        <translation>Razvijalci</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="DevelopersPage.qml" line="136"/>
@@ -37,31 +37,31 @@
     <message>
         <location filename="DevelopersPage.qml" line="141"/>
         <source>%1&apos;s Twitter account</source>
-        <translation>%1 Twitter račun</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="LegacyPatchPage.qml" line="49"/>
         <location filename="NewPatchPage.qml" line="49"/>
         <source>Patch being applied</source>
-        <translation>Popravek se namešča</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="LegacyPatchPage.qml" line="49"/>
         <location filename="NewPatchPage.qml" line="49"/>
         <source>Unapply patch</source>
-        <translation>Odstrani popravek</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="LegacyPatchPage.qml" line="49"/>
         <location filename="NewPatchPage.qml" line="49"/>
         <source>Apply patch</source>
-        <translation>Namesti popravek</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="LegacyPatchPage.qml" line="66"/>
         <location filename="WebPatchPage.qml" line="139"/>
         <source>Patch information</source>
-        <translation>Informacija o popravku</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="LegacyPatchPage.qml" line="76"/>
@@ -72,23 +72,23 @@
     <message>
         <location filename="LegacyPatchPage.qml" line="80"/>
         <source>Name</source>
-        <translation>Ime</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="LegacyPatchPage.qml" line="94"/>
         <source>Maintainer</source>
-        <translation>Skrbnik</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="LegacyPatchPage.qml" line="107"/>
         <location filename="NewPatchPage.qml" line="106"/>
         <source>Description</source>
-        <translation>Opis</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="NewPatchPage.qml" line="80"/>
         <source>Author</source>
-        <translation>Avtor</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="NewPatchPage.qml" line="93"/>
@@ -98,42 +98,42 @@
     <message>
         <location filename="PatchManagerPage.qml" line="43"/>
         <source>Browser</source>
-        <translation>Brskalnik</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="44"/>
         <source>Camera</source>
-        <translation>Kamera</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="45"/>
         <source>Calendar</source>
-        <translation>Koledar</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="46"/>
         <source>Clock</source>
-        <translation>Ura</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="47"/>
         <source>Contacts</source>
-        <translation>Stiki</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="48"/>
         <source>Email</source>
-        <translation>Pošta</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="49"/>
         <source>Gallery</source>
-        <translation>Galerija</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="50"/>
         <source>Homescreen</source>
-        <translation>Domači zaslon</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="51"/>
@@ -143,79 +143,79 @@
     <message>
         <location filename="PatchManagerPage.qml" line="52"/>
         <source>Messages</source>
-        <translation>Sporočila</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="53"/>
         <source>Phone</source>
-        <translation>Telefon</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="54"/>
         <source>Silica</source>
-        <translation>Silica</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="55"/>
         <source>Settings</source>
-        <translation>Nastavitve</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="56"/>
         <source>Other</source>
-        <translation>Ostalo</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="111"/>
         <source>Disable developer mode</source>
-        <translation>Onemogoči razvojni način</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="111"/>
         <source>Enable developer mode</source>
-        <translation>Omogoči razvojni način </translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="121"/>
         <location filename="WebCatalogPage.qml" line="83"/>
         <source>Web catalog</source>
-        <translation>Spletni katalog</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="127"/>
         <source>Restart preloaded services</source>
-        <translation>Ponovni zagon prednaloženih servisov</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="134"/>
         <source>Installed patches</source>
-        <translation>Nameščeni popravki</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="173"/>
         <source>This patch is not compatible with SailfishOS version!</source>
-        <translation>Ta popravek ni združljiv s trenutno različico SailfishOS!</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="196"/>
         <source>Uninstalling patch %1</source>
-        <translation>Odstranjevanje poravka %1</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="306"/>
         <source>Uninstall</source>
-        <translation>Odstrani</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="PatchManagerPage.qml" line="320"/>
         <location filename="WebCatalogPage.qml" line="187"/>
         <source>No patches available</source>
-        <translation>Ni popravkov</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="RestartServicesDialog.qml" line="47"/>
         <source>Restart services</source>
-        <translation>Ponovni zagon servisov</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="RestartServicesDialog.qml" line="53"/>
@@ -236,42 +236,42 @@
         <location filename="ScreenshotsPage.qml" line="39"/>
         <location filename="WebPatchPage.qml" line="326"/>
         <source>Screenshots</source>
-        <translation>Posnetki zaslona</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebCatalogPage.qml" line="72"/>
         <source>Hide search field</source>
-        <translation>Skrij polje za iskanje</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebCatalogPage.qml" line="72"/>
         <source>Show search field</source>
-        <translation>Prikaži polje za iskanje</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebCatalogPage.qml" line="83"/>
         <source>%1 patches</source>
-        <translation>%1 popravkov</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebCatalogPage.qml" line="89"/>
         <source>Tap to enter search query</source>
-        <translation>Pritisnite za vnos iskalnega niza</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="129"/>
         <source>Problem in fetching patch data</source>
-        <translation>Težava s pridobivanjem podatkov</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="242"/>
         <source>Author: %1</source>
-        <translation>Avtor: %1</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="258"/>
         <source>Links</source>
-        <translation>Povezave</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="279"/>
@@ -281,37 +281,37 @@
     <message>
         <location filename="WebPatchPage.qml" line="321"/>
         <source>Sources</source>
-        <translation>Viri</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="372"/>
         <source>Files</source>
-        <translation>Datoteke</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="388"/>
         <source>This file is not compatible with SailfishOS version!</source>
-        <translation>Datoteka ni združljiva s trenutno različico SailfishOS! </translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="390"/>
         <source>Install patch %1</source>
-        <translation>Namesti popravek %1</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="431"/>
         <source>[installed]</source>
-        <translation>[nameščeno]</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="431"/>
         <source>[click to install]</source>
-        <translation>[klikni za namestitev]</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="WebPatchPage.qml" line="446"/>
         <source>Compatible: %1</source>
-        <translation>Združljivo: %1</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
