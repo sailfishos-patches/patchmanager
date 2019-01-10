b0d51822f471605428ae1ca076c568f68438ccbd
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-01-11 02:44:41 +0800

    Create settings-patchmanager-zh_cn.ts
    Chinese translation

---
+++ translations/settings-patchmanager-zh_cn.ts
@@ -0,0 +1,482 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_cn" version="2.1">
+<context>
+    <name/>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="49"/>
+        <location filename="../src/qml/PatchManagerPage.qml" line="142"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/dialog/dialog.qml" line="45"/>
+        <source>Apply patches</source>
+        <translation>应用补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/dialog/dialog.qml" line="53"/>
+        <source>Patchmanager will automatically apply patches in 10 seconds.</source>
+        <translation>补丁管理会在10秒之内自动应用补丁。</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/dialog/dialog.qml" line="66"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/dialog/dialog.qml" line="28"/>
+        <source>Applying patches</source>
+        <translation>应用补丁中...</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/dialog/dialog.qml" line="136"/>
+        <source>Appled successfully!</source>
+        <translation>已成功应用!</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/dialog/dialog.qml" line="137"/>
+        <source>Failed applying patches!</source>
+        <translation>应用补丁失败!</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/dialog/dialog.qml" line="165"/>
+        <location filename="../src/qml/AboutPage.qml" line="63"/>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="179"/>
+        <source>Patchmanager</source>
+        <translation>补丁管理</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="71"/>
+        <source>Version: %1</source>
+        <translation>版本 %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="81"/>
+        <source>Patchmanager allows system modification via patches. It provides a system daemon that is in charge of performing those patches, as well as a GUI, to control those operations and installation/removal of patches.</source>
+        <translation>补丁管理允许通过补丁修改系统。它提供了一个负责执行这些补丁的系统守护程序, 以及GUI, 以控制这些操作和安装/移除补丁。</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="92"/>
+        <location filename="../src/qml/WebPatchPage.qml" line="246"/>
+        <source>Donate</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="103"/>
+        <location filename="../src/qml/DevelopersPage.qml" line="107"/>
+        <source>Developers</source>
+        <translation>开发者</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DevelopersPage.qml" line="146"/>
+        <source>%1&apos;s webpage</source>
+        <translation>网页%1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DevelopersPage.qml" line="151"/>
+        <source>%1&apos;s Twitter account</source>
+        <translation>%1 Twitter账户</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="51"/>
+        <location filename="../src/qml/NewPatchPage.qml" line="51"/>
+        <source>Patch being applied</source>
+        <translation>补丁正在被应用</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="51"/>
+        <location filename="../src/qml/NewPatchPage.qml" line="51"/>
+        <source>Unapply patch</source>
+        <translation>取消应用补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="51"/>
+        <location filename="../src/qml/NewPatchPage.qml" line="51"/>
+        <source>Apply patch</source>
+        <translation>应用补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="65"/>
+        <location filename="../src/qml/WebPatchPage.qml" line="85"/>
+        <source>Patch information</source>
+        <translation>补丁信息</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="75"/>
+        <location filename="../src/qml/NewPatchPage.qml" line="75"/>
+        <source>This patch is no available anymore. You won&apos;t be able to reinstall it.</source>
+        <translation>该补丁不再可用，你无法重装它。</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="79"/>
+        <source>Name</source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="93"/>
+        <source>Maintainer</source>
+        <translation>开发人员</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="107"/>
+        <location filename="../src/qml/NewPatchPage.qml" line="93"/>
+        <source>Possible conflicts</source>
+        <translation>可能的冲突</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="120"/>
+        <location filename="../src/qml/NewPatchPage.qml" line="133"/>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="132"/>
+        <location filename="../src/qml/NewPatchPage.qml" line="146"/>
+        <source>Patch log</source>
+        <translation>补丁日志</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/LegacyPatchPage.qml" line="141"/>
+        <location filename="../src/qml/NewPatchPage.qml" line="156"/>
+        <source>No log yet</source>
+        <translation>尚无日志</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewPatchPage.qml" line="79"/>
+        <source>Author</source>
+        <translation>作者</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewPatchPage.qml" line="107"/>
+        <source>Version</source>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewPatchPage.qml" line="120"/>
+        <source>Compatible</source>
+        <translation>兼容的</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="147"/>
+        <source>Unapply all patches</source>
+        <translation>取消应用全部补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="153"/>
+        <source>Load engine</source>
+        <translation>加载引擎</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="159"/>
+        <source>Updates available</source>
+        <translation>有可用更新</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="159"/>
+        <location filename="../src/qml/WebCatalogPage.qml" line="93"/>
+        <source>Web catalog</source>
+        <translation>网络目录</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="165"/>
+        <source>Restart preloaded services</source>
+        <translation>重启预加载服务</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="171"/>
+        <source>Resolve failure</source>
+        <translation>解决问题</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="178"/>
+        <source>Installed patches</source>
+        <translation>已安装补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="357"/>
+        <source>This patch is not compatible with SailfishOS version!</source>
+        <translation>该补丁不兼容当前旗鱼系统版本!</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="365"/>
+        <source>Uninstalling patch %1</source>
+        <translation>卸载补丁 %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="470"/>
+        <source>Compatible with:</source>
+        <translation>兼容:</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="484"/>
+        <source>Have possible conflicts</source>
+        <translation>可能有冲突</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="487"/>
+        <source>Patch info</source>
+        <translation>补丁信息</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="491"/>
+        <source>Unapply</source>
+        <translation>取消应用</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="491"/>
+        <source>Apply</source>
+        <translation>应用</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="496"/>
+        <source>Uninstall</source>
+        <translation>卸载</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="510"/>
+        <location filename="../src/qml/WebCatalogPage.qml" line="212"/>
+        <source>No patches available</source>
+        <translation>没有可用补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/RestartServicesDialog.qml" line="48"/>
+        <source>Restart services</source>
+        <translation>重启服务</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/RestartServicesDialog.qml" line="56"/>
+        <source>Some services will now be restarted. Phone interface might take time to load for a short moment.</source>
+        <translation>现在将会重启一些服务。可能需要一些时间以加载手机界面。</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ScreenshotsPage.qml" line="71"/>
+        <location filename="../src/qml/WebPatchPage.qml" line="272"/>
+        <source>Screenshots</source>
+        <translation>屏幕截图</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebCatalogPage.qml" line="82"/>
+        <source>Hide search field</source>
+        <translation>隐藏搜索字段</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebCatalogPage.qml" line="82"/>
+        <source>Show search field</source>
+        <translation>显示搜索字段</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebCatalogPage.qml" line="93"/>
+        <source>%1 patches</source>
+        <translation>%1补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebCatalogPage.qml" line="99"/>
+        <source>Tap to enter search query</source>
+        <translation>点击以进入搜索查询</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebCatalogPage.qml" line="199"/>
+        <source>Update available: %1</source>
+        <translation>可供安装更新: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="75"/>
+        <source>Fetching patch information...</source>
+        <translation>正在获取补丁信息...</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="75"/>
+        <source>Problem in fetching patch data</source>
+        <translation>获取补丁数据出错</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="188"/>
+        <source>Author: %1</source>
+        <translation>作者: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="204"/>
+        <source>Links</source>
+        <translation>链接</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="225"/>
+        <source>Open discussion link</source>
+        <translation>打开讨论链接</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="267"/>
+        <source>Sources</source>
+        <translation>源</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="318"/>
+        <source>Files</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="333"/>
+        <source>This file is not compatible with SailfishOS version!</source>
+        <translation>该文件不兼容当前旗鱼系统版本!</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="335"/>
+        <source>Install patch %1</source>
+        <translation>安装补丁 %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="388"/>
+        <source>[installed]</source>
+        <translation>[已安装]</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="388"/>
+        <source>[click to install]</source>
+        <translation>[点击以安装]</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WebPatchPage.qml" line="404"/>
+        <source>Compatible: %1</source>
+        <translation>兼容: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="190"/>
+        <source>Patch installed</source>
+        <translation>已安装补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="191"/>
+        <source>Patch %1 installed</source>
+        <translation>已安装 %1 补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="194"/>
+        <source>Patch removed</source>
+        <translation>已移除补丁</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="195"/>
+        <source>Patch %1 removed</source>
+        <translation>补丁 %1 已移除</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="198"/>
+        <source>Failed to install patch</source>
+        <translation>安装补丁失败</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="199"/>
+        <source>Patch %1 installation failed</source>
+        <translation>补丁 %1 安装失败</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="202"/>
+        <source>Failed to remove patch</source>
+        <translation>移除补丁失败</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="203"/>
+        <source>Patch %1 removal failed</source>
+        <translation>补丁 %1 移除失败</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="206"/>
+        <source>Update available</source>
+        <translation>有可供安装更新</translation>
+    </message>
+    <message>
+        <location filename="../src/bin/patchmanager-daemon/patchmanagerobject.cpp" line="207"/>
+        <source>Patch %1 have update candidate</source>
+        <translation>%1 补丁有候选更新</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/PatchManagerPage.qml" line="137"/>
+        <location filename="../src/qml/SettingsPage.qml" line="14"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="18"/>
+        <source>Apply on boot</source>
+        <translation>启动时应用</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="25"/>
+        <source>Developer mode</source>
+        <translation>开发者模式</translation>
+    </message>
+</context>
+<context>
+    <name>Sections</name>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="49"/>
+        <source>browser</source>
+        <translation>浏览器</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="50"/>
+        <source>camera</source>
+        <translation>相机</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="51"/>
+        <source>calendar</source>
+        <translation>日历</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="52"/>
+        <source>clock</source>
+        <translation>时钟</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="53"/>
+        <source>contacts</source>
+        <translation>联系人</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="54"/>
+        <source>email</source>
+        <translation>电子邮件</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="55"/>
+        <source>gallery</source>
+        <translation>图库</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="56"/>
+        <source>homescreen</source>
+        <translation>主屏幕</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="57"/>
+        <source>media</source>
+        <translation>媒体</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="58"/>
+        <source>messages</source>
+        <translation>信息</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="59"/>
+        <source>phone</source>
+        <translation>拨号</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="60"/>
+        <source>silica</source>
+        <translation>silica（硅）</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="61"/>
+        <source>settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/patchmanager.cpp" line="62"/>
+        <location filename="../src/qml/patchmanager.cpp" line="229"/>
+        <source>other</source>
+        <translation>其它</translation>
+    </message>
+</context>
+</TS>
