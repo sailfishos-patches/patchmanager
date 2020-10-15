0abeac840b07dc2189317d457a83139682ac6d66
olf <Olf0@users.noreply.github.com>
2020-10-15 22:00:46 +0200

    Adapt to d02ffc893b775e487e3b43bace212b2d173baecc
    See https://github.com/Olf0/patchmanager/commit/d02ffc893b775e487e3b43bace212b2d173baecc

--- translations/settings-patchmanager-sl.ts
+++ translations/settings-patchmanager-sl.ts
@@ -14,7 +14,7 @@
     </message>
     <message>
         <location filename="../src/bin/dialog/dialog.qml" line="53"/>
-        <source>Patchmanager will automatically apply patches in 10 seconds.</source>
+        <source>Patchmanager will automatically apply all patches in 10 seconds.</source>
         <translation>Patchmanager bo v 10. sekundah samodejno uveljavil popravke</translation>
     </message>
     <message>
@@ -29,12 +29,12 @@
     </message>
     <message>
         <location filename="../src/bin/dialog/dialog.qml" line="136"/>
-        <source>Appled successfully!</source>
-        <translation>Uspešno uveljavljeno!</translation>
+        <source>Applied successfully.</source>
+        <translation>Uspešno uveljavljeno.</translation>
     </message>
     <message>
         <location filename="../src/bin/dialog/dialog.qml" line="137"/>
-        <source>Failed applying patches!</source>
+        <source>Failed to apply patches!</source>
         <translation>Nespešno uveljavljanje popravkov!</translation>
     </message>
     <message>
