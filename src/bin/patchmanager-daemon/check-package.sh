844b2aa0db04c6be8a4da2108410087b0ef6ed5a
olf <Olf0@users.noreply.github.com>
2021-10-01 04:21:12 +0200

    [check-package.sh] Add proper quoting
    ... and omit a few superfluous curly braces, plus unify the syntax used for variable substitutions.

--- src/bin/patchmanager-daemon/check-package.sh
+++ src/bin/patchmanager-daemon/check-package.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if [ -e $1 ]; then
+if [ -e "$1" ]; then
   echo "Must provide package name"
   exit -1
 fi
 
-rpm -q $1 > /dev/null 2>&1
-if [ ${?} -ne 0 ]; then
+rpm -q "$1" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
   echo "Invalid package name"
   exit -1
 fi
@@ -55,26 +55,26 @@ rm -f $ORIGINAL_FAILED_TMP_FILE
 rm -f $BACKUP_FAILED_TMP_FILE
 rm -f $ORIGINAL_FAILED_FILE
 rm -f $BACKUP_FAILED_FILE
-rm -f $LOG_ORIGINAL_FILE
-rm -f $LOG_BACKUP_FILE
+rm -f "$LOG_ORIGINAL_FILE"
+rm -f "$LOG_BACKUP_FILE"
 
 mkdir -p /var/lib/patchmanager
 cd /
 
 # Verify original files in package
-rpm -q --queryformat '[%{FILEMD5S}  %{FILENAMES}\n]' $1 | grep \\.qml > $TMP_FILE
+rpm -q --queryformat '[%{FILEMD5S}  %{FILENAMES}\n]' "$1" | grep \\.qml > $TMP_FILE
 sha256sum -c $TMP_FILE 2> /dev/null | grep -v OK > $ORIGINAL_FAILED_TMP_FILE
 while read i; do
-  file=`echo ${i} |awk -F: '{print $1}'`
-  echo $file >> $ORIGINAL_FAILED_FILE
+  file="$(echo "$i" | awk -F: '{print $1}')"
+  echo "$file" >> $ORIGINAL_FAILED_FILE
 done < $ORIGINAL_FAILED_TMP_FILE
 
 # Verify backup files in package
-rpm -q --queryformat "[%{FILEMD5S}  $BACKUP_PREFIX%{FILENAMES}\n]" $1 | grep \\.qml > $TMP_FILE
+rpm -q --queryformat "[%{FILEMD5S}  $BACKUP_PREFIX%{FILENAMES}\n]" "$1" | grep \\.qml > $TMP_FILE
 sha256sum -c $TMP_FILE 2> /dev/null | grep -v OK > $BACKUP_FAILED_TMP_FILE
 while read i; do
-  file=`echo ${i} |awk -F: '{print $1}'`
-  echo $file >> $BACKUP_FAILED_FILE
+  file="$(echo "$i" | awk -F: '{print $1}')"
+  echo "$file" >> $BACKUP_FAILED_FILE
 done < $BACKUP_FAILED_TMP_FILE
 
 # If the backup is up-to-date, it's ok
@@ -87,28 +87,28 @@ fi
 # ok, we create the backup files
 
 if [ ! -f $ORIGINAL_FAILED_FILE ]; then
-  rpm -ql $1 | grep \\.qml > $TMP_FILE
+  rpm -ql "$1" | grep \\.qml > $TMP_FILE
   while read i; do
     backup_file="$BACKUP_PREFIX/$i"
-    rm -f $backup_file
-    backup_dir=$(dirname $backup_file)
-    mkdir -p $backup_dir
-    cp $i $backup_file
+    rm -f "$backup_file"
+    backup_dir="$(dirname "$backup_file")"
+    mkdir -p "$backup_dir"
+    cp "$i" "$backup_file"
   done < $TMP_FILE
   echo "Backup done"
   do_check_success
 fi
 
 while read i; do
-  file=`echo ${i} |awk -F: '{print $1}'`
-  echo $file >> $LOG_ORIGINAL_FILE
+  file="$(echo "$i" | awk -F: '{print $1}')"
+  echo "$file" >> $LOG_ORIGINAL_FILE
 done < $ORIGINAL_FAILED_TMP_FILE
 
 while read i; do
-  file=`echo ${i} |awk -F: '{print $1}'`
-  echo $file >> $LOG_BACKUP_FILE
+  file="$(echo "$i" | awk -F: '{print $1}')"
+  echo "$file" >> $LOG_BACKUP_FILE
 done < $BACKUP_FAILED_TMP_FILE
 
 
-echo "Warning ! Both backup and installed files are corrupted !"
+echo "Warning: Both backup and installed files are corrupted!"
 do_check_failure
