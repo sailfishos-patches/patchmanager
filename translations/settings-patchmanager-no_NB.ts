ec8cd8a1c03d0b4d08ace6c60d94358f1f7a7fbd
Frank Paul Silye <frankps@gmail.com>
2026-04-03 10:52:08 +0200

    Add files via upload

---
+++ translations/settings-patchmanager-no_NB.ts
@@ -0,0 +1,563 @@
+<!DOCTYPE QPH>
+<QPH language="no_NB">
+<phrase>
+    <source>Activate all enabled Patches</source>
+    <target>Aktiver alle aktiverte patcher</target>
+</phrase>
+<phrase>
+    <source>Patchmanager will start to activate all enabled Patches in %1 seconds.</source>
+    <target>Patchmanager starter aktivering av alle valgte patcher om %1 sekunder.</target>
+</phrase>
+<phrase>
+    <source>Quit</source>
+    <target>Avslutt</target>
+</phrase>
+<phrase>
+    <source>Activating all enabled Patches took %L1 seconds.</source>
+    <target>Aktivering av alle valgte patcher tok %L1 sekunder.</target>
+</phrase>
+<phrase>
+    <source>Successfully activated all enabled Patches.</source>
+    <target>Alle valgte patcher ble aktivert.</target>
+</phrase>
+<phrase>
+    <source>Failed to activate all enabled Patches!</source>
+    <target>Kunne ikke aktivere alle valgte patcher!</target>
+</phrase>
+<phrase>
+    <source>About Patchmanager</source>
+    <target>Om Patchmanager</target>
+</phrase>
+<phrase>
+    <source>Patchmanager</source>
+    <target>Patchmanager</target>
+</phrase>
+<phrase>
+    <source>Version: %1</source>
+    <target>Versjon: %1</target>
+</phrase>
+<phrase>
+    <source>Patchmanager allows to automatically modify system files via Patches. It provides a daemon which performs the activation of Patches, plus a GUI to configure these operations and to install or remove Patches.</source>
+    <target>Patchmanager gjør det mulig å endre systemfiler automatisk ved hjelp av patcher. Den inneholder en bakgrunnsprosess (daemon) som aktiverer patchene, i tillegg til et grafisk brukergrensesnitt (GUI) for å konfigurere disse operasjonene og for å installere eller fjerne patcher.</target>
+</phrase>
+<phrase>
+    <source>Licensed under the terms of the&lt;br /&gt;&lt;a href=&quot;%1&quot;&gt;BSD 3-Clause License&lt;/a&gt;</source>
+    <target>Lisensiert under vilkårene i<br /><a href="%1">BSD 3-Clause-lisensen</a></target>
+</phrase>
+<phrase>
+    <source>Source code repository and issue&lt;br /&gt;tracker for bug reports, feature&lt;br /&gt;suggestions and help requests&lt;br /&gt;&lt;a href=&quot;%1&quot;&gt;on GitHub&lt;/a&gt;</source>
+    <target>Kildekodelager og feilhåndteringssystem<br />for feilrapporter, forslag til funksjoner<br />og forespørsler om hjelp<br /><a href="%1">på GitHub</target>
+</phrase>
+<phrase>
+    <source>Credits and Acknowledgements&lt;br /&gt;&lt;a href=&quot;%1&quot;&gt;Developers&lt;/a&gt;</source>
+    <target></target>
+</phrase>
+<phrase>
+    <source>Donations</source>
+    <target>Donasjoner</target>
+</phrase>
+<phrase>
+    <source>If you appreciate our work, please consider a donation to help covering the hosting costs for OpenRepos. OpenRepos is critical infrastructure specifically for Patchmanager, because its Web Catalog of Patches is hosted there.</source>
+    <target>Hvis du setter pris på arbeidet vårt, kan du vurdere en donasjon for å hjelpe med å dekke driftskostnadene for OpenRepos. OpenRepos er kritisk infrastruktur for nettopp Patchmanager, ettersom nettkatalogen for patcher ligger der.</target>
+</phrase>
+<phrase>
+    <source>If for some reason you cannot donate to OpenRepos, we also appreciate donating to the Free Software Foundation Europe (FSFE).</source>
+    <target>Hvis du av en eller annen grunn ikke kan donere til OpenRepos, setter vi også pris på donasjoner til Free Software Foundation Europe (FSFE).</target>
+</phrase>
+<phrase>
+    <source>Developers</source>
+    <target>Utviklere</target>
+</phrase>
+<phrase>
+    <source>%1&apos;s webpage</source>
+    <target>%1s nettside</target>
+</phrase>
+<phrase>
+    <source>%1&apos;s %2 account</source>
+    <target>%1s %2-konto</target>
+</phrase>
+<phrase>
+    <source>Copied log to clipboard.</source>
+    <target>Kopierte logg til utklippstavle.</target>
+</phrase>
+<phrase>
+    <source>Activating Patch</source>
+    <target>Aktiverer patch</target>
+</phrase>
+<phrase>
+    <source>Deactivate Patch</source>
+    <target>Deaktiver patch</target>
+</phrase>
+<phrase>
+    <source>Activate Patch</source>
+    <target>Aktiver patch</target>
+</phrase>
+<phrase>
+    <source>Remove Patch</source>
+    <target>Fjern patch</target>
+</phrase>
+<phrase>
+    <source>Patch %1 removed.</source>
+    <target>Patch %1 fjernet.</target>
+</phrase>
+<phrase>
+    <source>Start Patchmanager&apos;s daemon before activating Patches</source>
+    <target>tart Patchmanager-daemonen før aktivering av patcher</target>
+</phrase>
+<phrase>
+    <source>This Patch is not available anymore. You will not be able to reinstall it.</source>
+    <target>Denne patchen er ikke lenger tilgjengelig. Du vil ikke kunne installere den på nytt</target>
+</phrase>
+<phrase>
+    <source>Author</source>
+    <target>Utvikler</target>
+</phrase>
+<phrase>
+    <source>Maintainer</source>
+    <target>Vedlikeholder</target>
+</phrase>
+<phrase>
+    <source>Version</source>
+    <target>Versjon</target>
+</phrase>
+<phrase>
+    <source>not available</source>
+    <target>ikke tilgjengelig</target>
+</phrase>
+<phrase>
+    <source>Compatible</source>
+    <target>Kompatibel</target>
+</phrase>
+<phrase>
+    <source>May conflict with these Patches:</source>
+    <target>Kan være i konflikt med følgende patcher:</target>
+</phrase>
+<phrase>
+    <source>Denne patchen bruker det gamle formatet for sin patch.json-fil. Hvis du er vedlikeholder, bør du vurdere å oppdatere til det nye formatet. Hvis du bruker Nettkatalog, skal du ikke inkludere en patch.json-fil i opplastingen din!<br />Se utviklerdelen i <a href="%1">README-filen</a> for detaljer..</source>
+    <target></target>
+</phrase>
+<phrase>
+    <source>Description</source>
+    <target>Beskrivelse</target>
+</phrase>
+<phrase>
+    <source>Discussion</source>
+    <target>Diskusjon</target>
+</phrase>
+<phrase>
+    <source>Source code repository</source>
+    <target>Repository for kildekode</target>
+</phrase>
+<phrase>
+    <source>Patch log</source>
+    <target>Patchlogg</target>
+</phrase>
+<phrase>
+    <source>Press and hold to copy log to the clipboard</source>
+    <target>Trykk og hold for å kopiere loggen til utklippstavlen</target>
+</phrase>
+<phrase>
+    <source>No log exists yet</source>
+    <target>Ingen logger enda</target>
+</phrase>
+<phrase>
+    <source>Disable and deactivate all Patches</source>
+    <target>Deaktiver og slå av alle patcher</target>
+</phrase>
+<phrase>
+    <source>Start Patchmanager&apos;s daemon</source>
+    <target>Start Patchmanager-daemonen</target>
+</phrase>
+<phrase>
+    <source>Updates available</source>
+    <target>Oppdateringer tilgjengelig</target>
+</phrase>
+<phrase>
+    <source>Web Catalog</source>
+    <target>Nettkatalkog</target>
+</phrase>
+<phrase>
+    <source>Restart preloaded services</source>
+    <target>Start forhåndslastede tjenester på nytt</target>
+</phrase>
+<phrase>
+    <source>Restore prior enabled list</source>
+    <target>Gjenopprett forrige aktiverte liste</target>
+</phrase>
+<phrase>
+    <source>Resolve failure</source>
+    <target>Løs feil</target>
+</phrase>
+<phrase>
+    <source>Installed Patches</source>
+    <target>Installerte patcher</target>
+</phrase>
+<phrase>
+    <source>This Patch is incompatible with the installed SailfishOS version.</source>
+    <target>Denne patchen er ikke kompatibel med den installerte versjonen av SailfishOS.</target>
+</phrase>
+<phrase>
+    <source>Removing Patch %1</source>
+    <target>Fjerner patch %1</target>
+</phrase>
+<phrase>
+    <source>Tap to configure</source>
+    <target>Trykk for å konfigurere</target>
+</phrase>
+<phrase>
+    <source>Tap to show configuration</source>
+    <target>Trykk for å vise konfigurasjon</target>
+</phrase>
+<phrase>
+    <source>Compatible with:</source>
+    <target>Kompatibelt med:</target>
+</phrase>
+<phrase>
+    <source>May conflict with another Patch, see %1</source>
+    <target>Kan være i konflikt med en annen patch, se %1</target>
+</phrase>
+<phrase>
+    <source>May conflict with %2 other Patches, see %1</source>
+    <target>Kan være i konflikt med %2 andre patcher, se %1</target>
+</phrase>
+<phrase>
+    <source>Details</source>
+    <target>Detaljer</target>
+</phrase>
+<phrase>
+    <source>Deactivate</source>
+    <target>Deaktiver</target>
+</phrase>
+<phrase>
+    <source>Activate</source>
+    <target>Aktiver</target>
+</phrase>
+<phrase>
+    <source>Remove</source>
+    <target>Fjern</target>
+</phrase>
+<phrase>
+    <source>No Patches available</source>
+    <target>Ingen patcher tilgjengelig</target>
+</phrase>
+<phrase>
+    <source>Restart</source>
+    <target>Start på nytt</target>
+</phrase>
+<phrase>
+    <source>Some services will be restarted now. Reloading the homescreen of the device might take a little time.</source>
+    <target>Enkelte tjenester vil nå bli startet på nytt. Det kan ta litt tid å laste inn startskjermen på enheten på nytt.</target>
+</phrase>
+<phrase>
+    <source>List of services:</source>
+    <target><Oversikt over tjenester:/target>
+</phrase>
+<phrase>
+    <source>Note that this will close all apps.</source>
+    <target><Merk at dette vil lukke alle apper./target>
+</phrase>
+<phrase>
+    <source>Note that this will close %1.</source>
+    <target>Merk at dette vil lukke %1.</target>
+</phrase>
+<phrase>
+    <source>Note that this will close the %1 app.</source>
+    <target>Merk at dette vil lukke appen %1</target>
+</phrase>
+<phrase>
+    <source>Screenshots</source>
+    <target>Skjermbilder</target>
+</phrase>
+<phrase>
+    <source>Hide search field</source>
+    <target>Skjul søkefelt</target>
+</phrase>
+<phrase>
+    <source>Show search field</source>
+    <target>Vis søkefelt</target>
+</phrase>
+<phrase>
+    <source>Sort by category</source>
+    <target>Sorter etter kategori</target>
+</phrase>
+<phrase>
+    <source>Sort by date updated</source>
+    <target>Sorter etter dato oppdatert</target>
+</phrase>
+<phrase>
+    <source>%1 Patches</source>
+    <target>%1 patcher</target>
+</phrase>
+<phrase>
+    <source>(by date updated)</source>
+    <target>(etter dato oppdatert)</target>
+</phrase>
+<phrase>
+    <source>(by category)</source>
+    <target>(etter kategori)</target>
+</phrase>
+<phrase>
+    <source>Tap to enter search query</source>
+    <target>Trykk for å søke</target>
+</phrase>
+<phrase>
+    <source>Update available: %1</source>
+    <target>Oppdatering tilgjengelig: %1</target>
+</phrase>
+<phrase>
+    <source>The Web Catalog website may be experiencing issues, or there is a problem connecting to the Internet.</source>
+    <target>Web Catalog-nettstedet kan oppleve problemer, eller det er problemer med tilkoblingen til internett.</target>
+</phrase>
+<phrase>
+    <source>Failed to fetch Patch data</source>
+    <target>Kunne ikke hente data for patch</target>
+</phrase>
+<phrase>
+    <source>Fetching patch data</source>
+    <target>Henter data for patch</target>
+</phrase>
+<phrase>
+    <source>Open Project Page</source>
+    <target>Åpne prosjektside</target>
+</phrase>
+<phrase>
+    <source>Author: %1</source>
+    <target>Utvikler: %1</target>
+</phrase>
+<phrase>
+    <source>Links</source>
+    <target>Lenker</target>
+</phrase>
+<phrase>
+    <source>Files</source>
+    <target>Filer</target>
+</phrase>
+<phrase>
+    <source>Install Patch %1</source>
+    <target>Installer patch %1</target>
+</phrase>
+<phrase>
+    <source>Re-Install Patch %1</source>
+    <target>Reinstaller patch %1</target>
+</phrase>
+<phrase>
+    <source>Patch details</source>
+    <target>Detaljer om patch</target>
+</phrase>
+<phrase>
+    <source>[tap to re-install]</source>
+    <target>[trykk for å reinstallere]</target>
+</phrase>
+<phrase>
+    <source>[installed]</source>
+    <target>[installert]</target>
+</phrase>
+<phrase>
+    <source>[tap to install]</source>
+    <target>[trykk for å installere]</target>
+</phrase>
+<phrase>
+    <source>Compatible: %1</source>
+    <target>Kompatibel: %1</target>
+</phrase>
+<phrase>
+    <source>Patch activated</source>
+    <target>Patch aktivert</target>
+</phrase>
+<phrase>
+    <source>Patch %1 activated</source>
+    <target>Patch %1 aktivert</target>
+</phrase>
+<phrase>
+    <source>some service(s) should be restarted.</source>
+    <target>noen tjenester bør startes på nytt.</target>
+</phrase>
+<phrase>
+    <source>Patch deactivated</source>
+    <target>Patch deaktivert</target>
+</phrase>
+<phrase>
+    <source>Patch %1 is now inactive.</source>
+    <target>Patch %1 er nå inaktiv.(</target>
+</phrase>
+<phrase>
+    <source>Failed to activate Patch</source>
+    <target>Kunne ikke aktivere patch<</target>
+</phrase>
+<phrase>
+    <source>Activating Patch %1 failed!</source>
+    <target>Aktivering av patch %1 mislyktes!</target>
+</phrase>
+<phrase>
+    <source>Failed to deactivate Patch</source>
+    <target>Kunne ikke deaktivere patch</target>
+</phrase>
+<phrase>
+    <source>Deactivating Patch %1 failed!</source>
+    <target>Deaktivering av patch %1 mislyktes!</target>
+</phrase>
+<phrase>
+    <source>Update available</source>
+    <target>Oppdatering tilgjengelig</target>
+</phrase>
+<phrase>
+    <source>An update for Patch %1 is available.</source>
+    <target>En oppdatering for patch %1 er tilgjengelig.</target>
+</phrase>
+<phrase>
+    <source>Settings</source>
+    <target>Innstillinger</target>
+</phrase>
+<phrase>
+    <source>General</source>
+    <target>Generelt</target>
+</phrase>
+<phrase>
+    <source>Show notification on success</source>
+    <target>Vis varsling ved suksess</target>
+</phrase>
+<phrase>
+    <source>If this is off, notifications will only be shown when something went wrong.</source>
+    <target>Hvis deaktivert, vises varslinger bare ved feil.</target>
+</phrase>
+<phrase>
+    <source>Show &apos;Disable and deactivate all Patches&apos; pulley menu entry</source>
+    <target>Vis "Deaktiver alle patcher" i menyen</target>
+</phrase>
+<phrase>
+    <source>Enable an additional pulley menu entry for Patchmanager&apos;s main page to disable and deactivate all Patches.</source>
+    <target>Aktiver et ekstra valg i menyen på Patchmanagers hovedside for å deaktivere alle patcher.</target>
+</phrase>
+<phrase>
+    <source>Show only updates in Web Catalog</source>
+    <target>Vis kun oppdateringer i nettkatalogen</target>
+</phrase>
+<phrase>
+    <source>When updates are available, hide all other Patches in Web Catalog.</source>
+    <target>Skjul alle andre patcher i nettkatalogen når oppdateringer er tilgjengelige.</target>
+</phrase>
+<phrase>
+    <source>Startup Activation</source>
+    <target>Aktivering ved oppstart</target>
+</phrase>
+<phrase>
+    <source>Activate Patches on first login</source>
+    <target>Aktiver patcher ved første pålogging</target>
+</phrase>
+<phrase>
+    <source>Automatically activate all enabled Patches after Home Screen has been started for the first time.</source>
+    <target>Aktiver alle valgte patcher automatisk første gang startskjermen har startet.</target>
+</phrase>
+<phrase>
+    <source>Activation Delay</source>
+    <target>Aktiveringsforsinkelse</target>
+</phrase>
+<phrase>
+    <source>%1 seconds</source>
+    <target>%1 sekunder</target>
+</phrase>
+<phrase>
+    <source>Activate Patches when booting</source>
+    <target>Aktiver patcher ved oppstart</target>
+</phrase>
+<phrase>
+    <source>Version Check</source>
+    <target>Versjonssjekk</target>
+</phrase>
+<phrase>
+    <source>Strict</source>
+    <target>Streng</target>
+</phrase>
+<phrase>
+    <source>No check</source>
+    <target>Ingen sjekk</target>
+</phrase>
+<phrase>
+    <source>Mode for Patch developers</source>
+    <target>Modus for patch-utviklere</target>
+</phrase>
+<phrase>
+    <source>Enable various functions to be used by Patch developers. Among other things, it shows debug log files for applying the patch file when a Patch is activated on its details page.</source>
+    <target>Aktiver ulike funksjoner for patch-utviklere. Dette viser blant annet loggfiler for feilsøking (debug) ved påføring av patch-filen når en patch aktiveres på sin detaljside.</target>
+</phrase>
+<phrase>
+    <source>Advanced</source>
+    <target>Avansert</target>
+</phrase>
+<phrase>
+    <source>Automatically activate all enabled Patches when SailfishOS starts.</source>
+    <target>Aktiver alle valgte patcher automatisk når SailfishOS starter.</target>
+</phrase>
+<phrase>
+    <source>Allow to enable Patches, which are not marked as compatible with the installed SailfishOS version. Note that Patches, which are actually incompatible, will not work.</source>
+    <target>Tillat aktivering av patcher som ikke er merket som kompatible med den installerte SailfishOS-versjonen. Merk at patcher som faktisk er inkompatible, ikke vil fungere.</target>
+</phrase>
+<phrase>
+    <source>Convert Patches between 32-bit and 64-bit</source>
+    <target>Konverter patcher mellom 32-bit og 64-bit</target>
+</phrase>
+<phrase>
+    <source>Automatically convert lib or lib64 for select paths shown below.</source>
+    <target>Konverter lib eller lib64 automatisk for de valgte banene nedenfor.</target>
+</phrase>
+<phrase>
+    <source>browser</source>
+    <target>Nettleser</target>
+</phrase>
+<phrase>
+    <source>camera</source>
+    <target>Kamera</target>
+</phrase>
+<phrase>
+    <source>calendar</source>
+    <target>Kalender</target>
+</phrase>
+<phrase>
+    <source>clock</source>
+    <target>Klokke</target>
+</phrase>
+<phrase>
+    <source>contacts</source>
+    <target>Kontakter</target>
+</phrase>
+<phrase>
+    <source>email</source>
+    <target>E-post</target>
+</phrase>
+<phrase>
+    <source>gallery</source>
+    <target>Galleri</target>
+</phrase>
+<phrase>
+    <source>homescreen</source>
+    <target>Startskjerm</target>
+</phrase>
+<phrase>
+    <source>media</source>
+    <target>Media</target>
+</phrase>
+<phrase>
+    <source>messages</source>
+    <target>Meldinger</target>
+</phrase>
+<phrase>
+    <source>phone</source>
+    <target>Telefon</target>
+</phrase>
+<phrase>
+    <source>silica</source>
+    <target>Silica</target>
+</phrase>
+<phrase>
+    <source>settings</source>
+    <target>Innstillinger</target>
+</phrase>
+<phrase>
+    <source>other</source>
+    <target>Annet</target>
+</phrase>
+<phrase>
+    <source>keyboard</source>
+    <target>Tastatur</target>
+</phrase>
+</QPH>
